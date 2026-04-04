f8d86973e575dce573939767d67f0f6bb5c9a065
Minidu dhananjana <minidudhananjana@gmail.com>
2026-04-04 23:38:45 +0530

    create docker images

--- ai-service/main.py
+++ ai-service/main.py
@@ -14,10 +14,8 @@ app = FastAPI(
 # Allow requests from React and Spring Boot
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:8080"
-    ],
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
